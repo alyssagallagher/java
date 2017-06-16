46dfe93b7088807cadc45b8cc674cfc544ea8cfa
Alyssa Gallagher <alyssagallagher@alyssas-air.lan>
2017-06-16 11:41:06 -0400

    skeleton set up

---
+++ ccti/src/main/java/CH1_ArraysStrings/Example.java
@@ -0,0 +1,19 @@
+package CH1_ArraysStrings;
+
+/**
+ * Created by alyssagallagher on 6/16/17.
+ */
+public class Example {
+
+    public void printExample() {
+        System.out.println("This is an example class setup!");
+    }
+
+
+    public static void main(String[] arr) {
+
+        Example e = new Example();
+        e.printExample();
+
+    }
+}
